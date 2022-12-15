f6830bbb1251b7bab71a7611b75e90060938035d
Muhammad Naseer <muhammadnaseerkhan1122@gmail.com>
2022-12-15 23:17:34 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/psl_eight/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.psl_eight
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PSL 8',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen()
+    );
+  }
+}
+
